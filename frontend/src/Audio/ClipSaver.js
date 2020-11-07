1adf3be1869828d45207ba4ce64b4869453eb05f
Mitesh Kumar <miteshkumarca@gmail.com>
2020-11-07 15:25:10 -0600

    Added firebase configs

--- frontend/src/Audio/ClipSaver.js
+++ frontend/src/Audio/ClipSaver.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { soundStorage } from '../helpers/helpers';
 
 function ClipSaver() {
     return (
--- frontend/src/helpers/storage.js
+++
@@ -1,5 +0,0 @@
-import { firebaseConfig } from '../config';
-import firebase from 'firebase';
-firebase.initializeApp(firebaseConfig);
-
-let storage = firebase.storage(); 
