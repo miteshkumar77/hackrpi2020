19cbf9c47bfab77ec38a33538986a19eb7d53c3e
jasonlam00 <lamj7@rpi.edu>
2020-11-07 19:10:05 -0500

    basic form auth with firebase

--- frontend/src/Signup.js
+++ frontend/src/Signup.js
@@ -1,9 +1,77 @@
 import React from 'react';
 import './Signup.css';
-import { signin, logout } from "../helpers/helpers.js";
-
+import firebase from 'firebase';
+import "firebase/auth";
+firebase.initializeApp(firebaseConfig);
 
 function Signup() {
+
+  function toggleSignIn() {
+      if (firebase.auth().currentUser) {
+        // [START signout]
+        firebase.auth().signOut();
+        // [END signout]
+      } else {
+        var email = document.getElementById('email').value;
+        var password = document.getElementById('password').value;
+        if (email.length < 4) {
+          alert('Please enter an email address.');
+          return;
+        }
+        if (password.length < 4) {
+          alert('Please enter a password.');
+          return;
+        }
+        // Sign in with email and pass.
+        // [START authwithemail]
+        firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+          // Handle Errors here.
+          var errorCode = error.code;
+          var errorMessage = error.message;
+          // [START_EXCLUDE]
+          if (errorCode === 'auth/wrong-password') {
+            alert('Wrong password.');
+          } else {
+            alert(errorMessage);
+          }
+          console.log(error);
+          document.getElementById('quickstart-sign-in').disabled = false;
+          // [END_EXCLUDE]
+        });
+        // [END authwithemail]
+      }
+      document.getElementById('quickstart-sign-in').disabled = true;
+    }
+
+  function handleSignUp() {
+        var email = document.getElementById('email').value;
+        var password = document.getElementById('password').value;
+        if (email.length < 4) {
+          alert('Please enter an email address.');
+          return;
+        }
+        if (password.length < 4) {
+          alert('Please enter a password.');
+          return;
+        }
+        // Create user with email and pass.
+        // [START createwithemail]
+        firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
+          // Handle Errors here.
+          var errorCode = error.code;
+          var errorMessage = error.message;
+          // [START_EXCLUDE]
+          if (errorCode == 'auth/weak-password') {
+            alert('The password is too weak.');
+          } else {
+            alert(errorMessage);
+          }
+          console.log(error);
+          // [END_EXCLUDE]
+        });
+        // [END createwithemail]
+      }
+
 
   return (
 
@@ -13,18 +81,17 @@ function Signup() {
        <div className="form-container sign-in-container">
           <form action="#">
              <h1>Sign in</h1>
-             <input type="email" placeholder="Email" />
-             <input type="password" placeholder="Password" />
-             <button>Sign In</button>
+             <input type="text" id="email" name="email" placeholder="Email" />
+             <input type="password" id="password" name="password" placeholder="Password" />
+             <button disabled onClick={toggleSignIn}>Sign In</button>
           </form>
        </div>
        <div className="overlay-container">
           <form action="#">
              <h1>Create Account</h1>
-             <input type="text" placeholder="Name" />
-             <input type="email" placeholder="Email" />
-             <input type="password" placeholder="Password" />
-             <button>Sign Up</button>
+             <input type="email" id="email" name="email" placeholder="Email" />
+             <input type="password" id="password" name="password" placeholder="Password" />
+             <button onClick={handleSignUp}>Sign Up</button>
           </form>
        </div>
     </div>
